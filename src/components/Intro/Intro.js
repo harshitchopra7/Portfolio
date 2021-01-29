e8be211f11c06a15f62f5aa2780ad3c8bbe40b13
Harshit Chopra <chopraharshit2000@gmail.com>
2021-01-29 11:40:40 +0530

    Updated and styled banner, intro and project description

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -11,15 +11,17 @@ function Intro() {
         <div className="intro">
             <div className="intro_div">
                 <div className="intro_left">
-                    <p className="intro_description">Hi! My name is Harshit Chopra and I am an avid web developer.</p>
+                    <div className="intro_description">
+                    <p>Hi! My name is Harshit Chopra and I am a passionate web developer fascinated by the tech world. I love to learn new things. Currently I am in my 3rd year of Btech in Computer Science Engineering.</p>
+                    
+                    </div>
                     <div>
                         <a href="https://www.linkedin.com/in/harshit-chopra-b98a94189/" target="_blank" rel="noreferrer"><img className="logos" src={linkedin} alt="" /></a>
                         <a href="mailto:chopraharshit2000@gmail.com"><img className="logos" src={gmail} alt="" /></a>
                         <a href="https://github.com/harshitchopra7" target="_blank" rel="noreferrer"><img className="logos" src={github} alt="" /></a>
                         <a href="https://www.hackerrank.com/chopraharshit201" target="_blank" rel="noreferrer"><img className="logos" src={hackerrank} alt="" /></a>        
                     </div>
-                    <div>
-                    </div>
+                    
                 </div>
                 <div className="intro_right">
                     <img className="harshit" src={harshit} alt="" />
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -6,12 +6,12 @@ function Projects() {
         <div>
             <div className="projects"></div>
             <div className="project_description">
-                <h3>Ecommerce Website</h3>
+                <h3 className="color_coral">Ecommerce Website</h3>
                 <p>
                 A functional E-commerce website built using html, css, javascript, react.js, reducer and is hosted on firebase.
                 Also, it has all the authentication for login as well as signup.
                 </p>
-                <h3>Link - kcart.web.app</h3>
+                <h3 className="color_coral">Link - <a href="https://k-cart.web.app/" target="_blank" rel="noreferrer" className="color_coral">kcart.web.app</a></h3>
             </div>
             
         </div>
